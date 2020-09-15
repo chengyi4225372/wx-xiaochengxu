402c978072ddd5ace6c917fb3cd5a214c0aa1b5d
addzero <1142484045@qq.com>
2020-09-15 17:29:32 +0800

    提交

--- pages/deed_diaryinfo/diaryinfo.js
+++ pages/deed_diaryinfo/diaryinfo.js
@@ -110,18 +110,49 @@ Page({
 
     }
   },
+  getInfo() {
+    var that = this;
+    wx.request({
+      url: getApp().globalData.deeds_info,
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      data: {
+        id: this.data.id
+      },
+      success: function (res) {
+        // success
+        console.log(res.data.data);
+
+        that.setData({
+          // diaryList: res.data.data,
+          contactinfo: res.data.data.content,
+          h1: res.data.data.title
+        })
+
+        WxParse.wxParse('content', 'html', res.data.data.content, that);
+
+        wx.setStorageSync('diaryList', res.data.data);
+
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
     var that = this;
-    var data = {
-      contactinfo: '<p style="text-indent:2em;">全面实现小康，少数民族一个都不能少，一个都不能掉队，要以时不我待的担当精神，创新工作思路，加大扶持力度，因地制宜，精准发力。</p><p style="text-indent:2em;">营销新浪潮·第五届移动互联网营销峰会|鸟哥笔记 : 活动行提供营销新浪潮·第五届移动互联网营销峰会|鸟哥笔记门票优惠。营销新浪潮·第五届移动互联网营销峰会|鸟哥笔记由（鸟哥笔记）在上海举办，预约报名截止（2020/1/14 17:30:00）。一键查询（营销新浪潮·第五届移动互联网营销峰会|鸟哥笔记）相关信息，包含时间、 地点、日程、价格等信息，在线报名，轻松快捷。</p><br/><img src="../../images/img8.png"/>'
+    var _id;
+    if (options.id) {
+      that.setData({
+        id: options.id
+      })
+      that.getInfo()
     }
-    that.setData({
-      contactinfo: data.contactinfo
-    })
-    WxParse.wxParse('content', 'html', data.contactinfo, that);
     // 音频初始化
     const bgM = wx.createInnerAudioContext();
     bgM.src = this.data.audioSrc;
